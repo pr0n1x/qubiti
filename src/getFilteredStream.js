c144f37ef90296a5731251fc76720f330cd2d270
pr0n1x <pr0n1x@yandex.ru>
2020-06-21 14:39:36 +0700

    Удален забытый отладочный вывод

--- src/getFilteredStream.js
+++ src/getFilteredStream.js
@@ -4,7 +4,6 @@ const through2 = require('through2')
 function getFilteredStream(flt) {
 	if (typeof flt === 'string' && flt.length > 0) {
 		flt = flt.replace(/\*\*\*/g, '**/{,.*/**}');
-		console.log('flt', flt);
 		return filter(flt);
 	}
 	return through2.obj();
